c0e9033d290fec66e95566cb7b19a9a9cf66f03d
EchoDev <EchoDev@users.noreply.github.com>
2020-12-16 20:14:28 +0100

    Disable 2660

--- user.js
+++ user.js
@@ -1216,8 +1216,8 @@ user_pref("browser.download.hide_plugins_without_extensions", false);
  * [SETUP-CHROME] This will break extensions, language packs, themes and any other
  * XPI files which are installed outside of profile and application directories
  * [1] https://mike.kaply.com/2012/02/21/understanding-add-on-scopes/
- * [1] archived: https://archive.is/DYjAM ***/
-user_pref("extensions.enabledScopes", 5); // [HIDDEN PREF]
+ * [1] archived: https://archive.is/DYjAM 
+ * user_pref("extensions.enabledScopes", 5); // [HIDDEN PREF]***/
 user_pref("extensions.autoDisableScopes", 15); // [DEFAULT: 15]
 /* 2662: disable webextension restrictions on certain mozilla domains (you also need 4503) [FF60+]
  * [1] https://bugzilla.mozilla.org/buglist.cgi?bug_id=1384330,1406795,1415644,1453988 ***/
